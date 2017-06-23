31c7a057c4ab11a7722dbf3a5ff0fbf0e58f9cdd
Hendrik Kahlen <hendrik.kahlen@uni-oldenburg.de>
2017-06-23 15:05:58 +0200

    #5 Minimized Tree of a network implemented correctly. Using Kruskal as a second approach.

---
+++ src/main/java/de/uni_oldenburg/transport/optimizers/Graph/Kruskal.java
@@ -0,0 +1,7 @@
+package de.uni_oldenburg.transport.optimizers.Graph;
+
+/**
+ * Created by Icebreaker on 23.06.2017.
+ */
+public class Kruskal {
+}
--- src/main/java/de/uni_oldenburg/transport/optimizers/NorthWestCornerOptimizer.java
+++ src/main/java/de/uni_oldenburg/transport/optimizers/NorthWestCornerOptimizer.java
@@ -15,7 +15,6 @@ public class NorthWestCornerOptimizer implements Optimizer {
 	@Override
 	public Solution optimizeTransportNetwork(TransportNetwork transportNetwork) {
 
-		//ArrayList<ArrayList<Location>> networkCycles = getNetworkCycles(transportNetwork.getLocations(), transportNetwork.getStartLocation());
 		ArrayList<ArrayList<Vertice>> minimalSpanningNetwork = getMinimalSpanningNetwork(transportNetwork.getLocations(), transportNetwork.getStartLocation());
 		for (ArrayList<Vertice> spanningNode : minimalSpanningNetwork) {
 			for (int i = 0; i < spanningNode.size(); i++) {
@@ -43,9 +42,40 @@ public class NorthWestCornerOptimizer implements Optimizer {
 			}
 		}
 
-		System.out.println(allVerticesGot(locations, spanningNodes));
+		ArrayList<ArrayList<Vertice>> spanningNodesOut = new ArrayList<>(spanningNodes);
+		for (int i = 0; i < spanningNodes.size(); i++) {
+			ArrayList<Vertice> spanningNode = spanningNodes.get(i);
+			for (int y = 0; y < spanningNodes.size(); y++) {
+				if (y != i) {
+					minimize(spanningNode, spanningNodes.get(y), spanningNodesOut);
+				}
+			}
+		}
 
-		return spanningNodes;
+		if (allVerticesGot(locations, spanningNodes)) {
+			return spanningNodesOut;
+		} else {
+			System.out.println("Failed to get shortest paths");
+			return null;
+		}
+
+	}
+
+	private void minimize(ArrayList<Vertice> spanningNode, ArrayList<Vertice> vertices, ArrayList<ArrayList<Vertice>> spanningNodesOut) {
+		boolean replacable = true;
+
+		int min = Math.min(spanningNode.size(), vertices.size());
+		for (int i = 0; i < min; i++) {
+			if (!spanningNode.get(i).getLocationReference().getName().equals(vertices.get(i).getLocationReference().getName())) {
+				replacable = false;
+			}
+		}
+
+		if (replacable && min == spanningNode.size()) {
+			spanningNodesOut.remove(spanningNode);
+		} else if (replacable) {
+			spanningNodesOut.remove(vertices);
+		}
 	}
 
 	private void findNewSpanningNode(ArrayList<ArrayList<Vertice>> spanningNodesTmp, ArrayList<Vertice> spanningNode, ArrayList<Vertice> alreadyVisitedList, Vertice startLocation, String hamburg, int deepth) {
@@ -67,7 +97,7 @@ public class NorthWestCornerOptimizer implements Optimizer {
 					findNewSpanningNode(spanningNodesTmp, new ArrayList<Vertice>(spanningNode) /*Create a copy*/, new ArrayList<>(alreadyVisitedList), location, hamburg, deepth - 1);
 				} else {
 					// cycle
-					spanningNodesTmp.add(spanningNode); // TODO remove
+					//spanningNodesTmp.add(spanningNode); // TODO imlement cycle detection?!
 				}
 			}
 		} else {
@@ -84,9 +114,6 @@ public class NorthWestCornerOptimizer implements Optimizer {
 			int oldWeigth = 0;
 			int newWeigth = 0;
 
-			if (spanningNode.get(spanningNode.size() - 1).getLocationReference().getName().equals("Dresden") && spanningNodeAlreadyGot.get(spanningNodeAlreadyGot.size() - 1).getLocationReference().getName().equals("Dresden")) {
-				System.currentTimeMillis();
-			}
 			if (spanningNode.get(spanningNode.size() - 1).getLocationReference().getName().equals(spanningNodeAlreadyGot.get(spanningNodeAlreadyGot.size() - 1).getLocationReference().getName())) {
 				matchFound = true;
 				// because all nodes start from the original start location the end specifies the node
@@ -98,108 +125,14 @@ public class NorthWestCornerOptimizer implements Optimizer {
 				if (spanningNode.get(spanningNode.size() - comparedMinimum).getLocationReference().getName().equals("Hamburg")
 						&& spanningNodeAlreadyGot.get(spanningNodeAlreadyGot.size() - comparedMinimum).getLocationReference().getName().equals("Hamburg")) { // only compare paths that are of same length // TODO find a better solution to switch paths and replace parts of them
 					if (oldWeigth > newWeigth) {
-						spanningNodes.remove(spanningNodeAlreadyGot); // TODO do not delete but adjust the path
+						//spanningNodes.remove(spanningNodeAlreadyGot); // TODO do not delete but adjust the path
 						return true;
 					}
 				}
+
 			}
 		}
 		return !matchFound;
-	}
-
-	private ArrayList<ArrayList<Location>> getNetworkCycles(Location[] locations, Location startLocation) {
-		ArrayList<ArrayList<Location>> cycles = new ArrayList<>();
-
-		int counter = 0;
-		do {
-			ArrayList<Location> cycle = new ArrayList<>();
-			findNewCycle(cycle, new ArrayList<>(), startLocation, "Hamburg", counter);
-			if (!cycleAlreadyExists(cycle, cycles)) {
-				for (Location location : cycle) {
-					System.out.print(location.getName() + ", ");
-				}
-				System.out.print("\n");
-				cycles.add(cycle);
-			} else {
-				counter++;
-			}
-		} while (!allLocationsGot(locations, cycles) && counter < locations.length);
-		return cycles;
-	}
-
-	private boolean findNewCycle(ArrayList<Location> locations, ArrayList<Location> alreadyVisitedList, Location startLocation, String originalLocationName, int counter) {
-		alreadyVisitedList.add(startLocation);
-		locations.add(startLocation);
-
-		if (startLocation.getNeighbouringLocations().size() == 1) {
-			alreadyVisitedList.remove(locations.get(locations.size() - 2)); // remove the original start that led into the one way
-		}
-
-		int i = 0;
-		for (Map.Entry<Location, Integer> neighbouringLocation : startLocation.getNeighbouringLocations().entrySet()) {
-			Location location = neighbouringLocation.getKey();
-			if (startLocation.getName().equals("Hamburg") && location.getName().equals("Kiel")) {
-				System.currentTimeMillis();
-			}
-			boolean alreadyVisited = alreadyVisited(location, alreadyVisitedList);
-
-			if (!alreadyVisited && i >= counter) {
-				if (findNewCycle(locations, alreadyVisitedList, location, originalLocationName, counter)) {
-					return true;
-				}
-			} else if (location.getName().equals(originalLocationName) && locations.size() != 2) {
-				return true;
-			} else {
-				i++;
-			}
-		}
-		return false;
-	}
-
-	private boolean cycleAlreadyExists(ArrayList<Location> cycle, ArrayList<ArrayList<Location>> cycles) {
-		boolean wasAlwaysFalse = false;
-		for (ArrayList<Location> alreadyGotCycle : cycles) {
-			boolean isEqual = true;
-			for (int i = 0; i < Math.min(cycle.size(), alreadyGotCycle.size()); i++) {
-				if (!cycle.get(i).getName().equals(alreadyGotCycle.get(i).getName())) isEqual = false;
-			}
-			if (isEqual) wasAlwaysFalse = true;
-		}
-		// TODO check inverse
-		for (ArrayList<Location> alreadyGotCycle : cycles) {
-			boolean isEqual = true;
-			for (int i = 0; i < Math.min(cycle.size(), alreadyGotCycle.size()); i++) {
-				if (!cycle.get(i).getName().equals(alreadyGotCycle.get(Math.min(cycle.size(), alreadyGotCycle.size()) - i - 1).getName()))
-					isEqual = false;
-			}
-			if (isEqual) wasAlwaysFalse = true;
-		}
-		return wasAlwaysFalse;
-	}
-
-	private boolean allLocationsGot(Location[] locations, ArrayList<ArrayList<Location>> cycles) {
-		boolean[] locationsGot = new boolean[locations.length];
-		for (ArrayList<Location> cycle : cycles) {
-			for (Location location : cycle) {
-				for (int i = 0; i < locations.length; i++) {
-					if (locations[i].getName().equals(location.getName())) {
-						locationsGot[i] = true;
-						break;
-					}
-				}
-			}
-			boolean allLocationsGot = true;
-			for (boolean aLocationsGot : locationsGot) {
-				if (!aLocationsGot) {
-					allLocationsGot = false;
-					break;
-				}
-			}
-			if (allLocationsGot) {
-				return allLocationsGot;
-			}
-		}
-		return false;
 	}
 
 	private boolean allVerticesGot(Location[] locations, ArrayList<ArrayList<Vertice>> spanningNodes) {
