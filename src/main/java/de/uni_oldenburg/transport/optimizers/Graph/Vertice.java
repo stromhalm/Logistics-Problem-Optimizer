caadd54fb5f99bdd88f902b4981f8fad9b38df4e
Hendrik Kahlen <hendrik.kahlen@uni-oldenburg.de>
2017-06-23 13:50:44 +0200

    #5 Minimal spanning tree added.

---
+++ src/main/java/de/uni_oldenburg/transport/optimizers/Graph/Vertice.java
@@ -0,0 +1,58 @@
+package de.uni_oldenburg.transport.optimizers.Graph;
+
+import de.uni_oldenburg.transport.Location;
+
+import java.util.ArrayList;
+
+public class Vertice {
+
+	private ArrayList<Vertice> childNodes;
+
+	private Vertice parentLocation;
+	private int expenseToParentLocation;
+
+	Location locationReference;
+
+	public Vertice(Location location, Vertice parentLocation, int expenseToParentLocation) {
+		this.locationReference = location;
+		this.parentLocation = parentLocation;
+		this.expenseToParentLocation = expenseToParentLocation;
+		this.childNodes = new ArrayList<>();
+	}
+
+	public boolean addChild(Vertice child) {
+		return this.childNodes.add(child);
+	}
+
+	public ArrayList<Vertice> getChildNodes() {
+		return childNodes;
+	}
+
+	public void setChildNodes(ArrayList<Vertice> childNodes) {
+		this.childNodes = childNodes;
+	}
+
+	public Vertice getParentLocation() {
+		return parentLocation;
+	}
+
+	public void setParentLocation(Vertice parentLocation) {
+		this.parentLocation = parentLocation;
+	}
+
+	public int getExpenseToParentLocation() {
+		return expenseToParentLocation;
+	}
+
+	public void setExpenseToParentLocation(int expenseToParentLocation) {
+		this.expenseToParentLocation = expenseToParentLocation;
+	}
+
+	public Location getLocationReference() {
+		return locationReference;
+	}
+
+	public void setLocationReference(Location locationReference) {
+		this.locationReference = locationReference;
+	}
+}
--- src/main/java/de/uni_oldenburg/transport/optimizers/NorthWestCornerOptimizer.java
+++ src/main/java/de/uni_oldenburg/transport/optimizers/NorthWestCornerOptimizer.java
@@ -3,8 +3,8 @@ package de.uni_oldenburg.transport.optimizers;
 import de.uni_oldenburg.transport.Location;
 import de.uni_oldenburg.transport.Solution;
 import de.uni_oldenburg.transport.TransportNetwork;
+import de.uni_oldenburg.transport.optimizers.Graph.Vertice;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Map;
 
@@ -15,9 +15,96 @@ public class NorthWestCornerOptimizer implements Optimizer {
 	@Override
 	public Solution optimizeTransportNetwork(TransportNetwork transportNetwork) {
 
-		ArrayList<ArrayList<Location>> networkCycles = getNetworkCycles(transportNetwork.getLocations(), transportNetwork.getStartLocation());
-
+		//ArrayList<ArrayList<Location>> networkCycles = getNetworkCycles(transportNetwork.getLocations(), transportNetwork.getStartLocation());
+		ArrayList<ArrayList<Vertice>> minimalSpanningNetwork = getMinimalSpanningNetwork(transportNetwork.getLocations(), transportNetwork.getStartLocation());
+		for (ArrayList<Vertice> spanningNode : minimalSpanningNetwork) {
+			for (int i = 0; i < spanningNode.size(); i++) {
+				System.out.print(spanningNode.get(i).getLocationReference().getName() + ", ");
+			}
+			System.out.print("\n");
+		}
 		return null;
+	}
+
+	private ArrayList<ArrayList<Vertice>> getMinimalSpanningNetwork(Location[] locations, Location startLocation) {
+		ArrayList<ArrayList<Vertice>> spanningNodes = new ArrayList<>();
+
+
+		for (int deepth = 0; deepth < locations.length; deepth++) {
+			Vertice root = new Vertice(startLocation, null, 0);
+
+			ArrayList<ArrayList<Vertice>> spanningNodesTmp = new ArrayList<>();
+			ArrayList<Vertice> spanningNodeTmp = new ArrayList<>();
+			findNewSpanningNode(spanningNodesTmp, spanningNodeTmp, new ArrayList<Vertice>(), root, "Hamburg", deepth);
+			for (ArrayList<Vertice> spanningNode : spanningNodesTmp) {
+				if (spanningNodeIsCheaperOrNew(spanningNode, spanningNodes)) {
+					spanningNodes.add(spanningNode);
+				}
+			}
+		}
+
+		System.out.println(allVerticesGot(locations, spanningNodes));
+
+		return spanningNodes;
+	}
+
+	private void findNewSpanningNode(ArrayList<ArrayList<Vertice>> spanningNodesTmp, ArrayList<Vertice> spanningNode, ArrayList<Vertice> alreadyVisitedList, Vertice startLocation, String hamburg, int deepth) {
+
+		spanningNode.add(startLocation);
+		alreadyVisitedList.add(startLocation);
+		if (deepth != 0) {
+			for (Map.Entry<Location, Integer> neighbouringLocation : startLocation.getLocationReference().getNeighbouringLocations().entrySet()) {
+				Vertice location = new Vertice(neighbouringLocation.getKey(), startLocation, neighbouringLocation.getValue());
+				if (!alreadyVisited(location, alreadyVisitedList)) {
+
+					if (location.getLocationReference().getName().equals(hamburg)) {
+						return;
+					}
+					startLocation.addChild(location);
+					int weight = neighbouringLocation.getValue();
+					location.setExpenseToParentLocation(weight);
+
+					findNewSpanningNode(spanningNodesTmp, new ArrayList<Vertice>(spanningNode) /*Create a copy*/, new ArrayList<>(alreadyVisitedList), location, hamburg, deepth - 1);
+				} else {
+					// cycle
+					spanningNodesTmp.add(spanningNode); // TODO remove
+				}
+			}
+		} else {
+			// end is reached
+			spanningNodesTmp.add(spanningNode);
+
+		}
+	}
+
+	private boolean spanningNodeIsCheaperOrNew(ArrayList<Vertice> spanningNode, ArrayList<ArrayList<Vertice>> spanningNodes) {
+
+		boolean matchFound = false;
+		for (ArrayList<Vertice> spanningNodeAlreadyGot : spanningNodes) {
+			int oldWeigth = 0;
+			int newWeigth = 0;
+
+			if (spanningNode.get(spanningNode.size() - 1).getLocationReference().getName().equals("Dresden") && spanningNodeAlreadyGot.get(spanningNodeAlreadyGot.size() - 1).getLocationReference().getName().equals("Dresden")) {
+				System.currentTimeMillis();
+			}
+			if (spanningNode.get(spanningNode.size() - 1).getLocationReference().getName().equals(spanningNodeAlreadyGot.get(spanningNodeAlreadyGot.size() - 1).getLocationReference().getName())) {
+				matchFound = true;
+				// because all nodes start from the original start location the end specifies the node
+				int comparedMinimum = (Math.min(spanningNode.size(), spanningNodeAlreadyGot.size()) - 1);
+				for (int i = comparedMinimum; i > 0; i--) { // compare which one is cheaper
+					newWeigth += spanningNode.get(spanningNode.size() - i).getExpenseToParentLocation();
+					oldWeigth += spanningNodeAlreadyGot.get(spanningNodeAlreadyGot.size() - i).getExpenseToParentLocation();
+				}
+				if (spanningNode.get(spanningNode.size() - comparedMinimum).getLocationReference().getName().equals("Hamburg")
+						&& spanningNodeAlreadyGot.get(spanningNodeAlreadyGot.size() - comparedMinimum).getLocationReference().getName().equals("Hamburg")) { // only compare paths that are of same length // TODO find a better solution to switch paths and replace parts of them
+					if (oldWeigth > newWeigth) {
+						spanningNodes.remove(spanningNodeAlreadyGot); // TODO do not delete but adjust the path
+						return true;
+					}
+				}
+			}
+		}
+		return !matchFound;
 	}
 
 	private ArrayList<ArrayList<Location>> getNetworkCycles(Location[] locations, Location startLocation) {
@@ -32,10 +119,10 @@ public class NorthWestCornerOptimizer implements Optimizer {
 					System.out.print(location.getName() + ", ");
 				}
 				System.out.print("\n");
+				cycles.add(cycle);
 			} else {
 				counter++;
 			}
-			cycles.add(cycle);
 		} while (!allLocationsGot(locations, cycles) && counter < locations.length);
 		return cycles;
 	}
@@ -51,7 +138,7 @@ public class NorthWestCornerOptimizer implements Optimizer {
 		int i = 0;
 		for (Map.Entry<Location, Integer> neighbouringLocation : startLocation.getNeighbouringLocations().entrySet()) {
 			Location location = neighbouringLocation.getKey();
-			if (startLocation.getName().equals("Hamburg") && location.getName().equals("Kiel")){
+			if (startLocation.getName().equals("Hamburg") && location.getName().equals("Kiel")) {
 				System.currentTimeMillis();
 			}
 			boolean alreadyVisited = alreadyVisited(location, alreadyVisitedList);
@@ -110,6 +197,40 @@ public class NorthWestCornerOptimizer implements Optimizer {
 			}
 			if (allLocationsGot) {
 				return allLocationsGot;
+			}
+		}
+		return false;
+	}
+
+	private boolean allVerticesGot(Location[] locations, ArrayList<ArrayList<Vertice>> spanningNodes) {
+		boolean[] locationsGot = new boolean[locations.length];
+		for (ArrayList<Vertice> nodes : spanningNodes) {
+			for (Vertice location : nodes) {
+				for (int i = 0; i < locations.length; i++) {
+					if (locations[i].getName().equals(location.getLocationReference().getName())) {
+						locationsGot[i] = true;
+						break;
+					}
+				}
+			}
+			boolean allLocationsGot = true;
+			for (boolean aLocationsGot : locationsGot) {
+				if (!aLocationsGot) {
+					allLocationsGot = false;
+					break;
+				}
+			}
+			if (allLocationsGot) {
+				return allLocationsGot;
+			}
+		}
+		return false;
+	}
+
+	private boolean alreadyVisited(Vertice location, ArrayList<Vertice> alreadyVisitedList) {
+		for (Vertice vertice : alreadyVisitedList) {
+			if (location.getLocationReference().getName().equals(vertice.getLocationReference().getName())) {
+				return true;
 			}
 		}
 		return false;
