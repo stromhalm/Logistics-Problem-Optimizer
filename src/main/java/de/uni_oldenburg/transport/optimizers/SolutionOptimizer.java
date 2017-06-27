7f940081bcd7ca9afcea7c493eae2454db148451
Adrian <adrian.jagusch@we-mail.de>
2017-06-28 01:44:13 +0200

    Merge remote-tracking branch 'remotes/origin/master' into pheromone-improvements
    # Conflicts:
    #	src/main/java/de/uni_oldenburg/transport/AppStarter.java
    #	src/main/java/de/uni_oldenburg/transport/TourDestination.java

--- src/main/java/de/uni_oldenburg/transport/optimizers/SolutionOptimizer.java
+++ src/main/java/de/uni_oldenburg/transport/optimizers/SolutionOptimizer.java
@@ -31,7 +31,7 @@ public class SolutionOptimizer implements Optimizer {
 		for (Tour tour : allTours) {
 			solution.removeTour(tour);
 
-			if (solution.isValid() && solution.getConsumption() < consumptionBefore) {
+			if (solution.isValid() && solution.getConsumption() < consumptionBefore) { // TODO: WTF?!
 				break;
 			}
 
