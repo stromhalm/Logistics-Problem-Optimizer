7b801bf479d1ce135b3754e5e1075d90c74832d6
Hendrik Kahlen <hendrik.kahlen@uni-oldenburg.de>
2017-07-02 16:51:42 +0200

    ClassDiagrams added.

--- src/main/java/de/uni_oldenburg/transport/optimizers/SavingsOptimizer.java
+++ src/main/java/de/uni_oldenburg/transport/optimizers/SavingsOptimizer.java
@@ -1,7 +1,14 @@
 package de.uni_oldenburg.transport.optimizers;
 
+import de.uni_oldenburg.transport.Solution;
+import de.uni_oldenburg.transport.TransportNetwork;
+
 /**
  * Created by lea on 02.07.17.
  */
-public class SavingsOptimizer {
+public class SavingsOptimizer implements Optimizer {
+	@Override
+	public Solution optimizeTransportNetwork(TransportNetwork transportNetwork) {
+		return null;
+	}
 }
